47317029bbfe97f3cbfd0ebcc560afbd4e19796b
CubaseAdida <kedornyota328@gmail.com>
2026-04-30 12:28:05 +0100

    fix database path for render

--- config.py
+++ config.py
@@ -3,8 +3,14 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+basedir = os.path.abspath(os.path.dirname(__file__))
+instance_path = os.path.join(basedir, "instance")
+
+# créer le dossier si nécessaire
+os.makedirs(instance_path, exist_ok=True)
+
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'dev-secret-key-change-in-prod'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///instance/stores.db'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        "sqlite:///" + os.path.join(instance_path, "stores.db")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-
